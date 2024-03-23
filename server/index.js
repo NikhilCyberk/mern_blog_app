2331f529b8c4525eb996645ff648a330c160c038
NikhilCyberk <knikhil128@gmail.com>
2024-03-23 19:29:46 +0530

    create signup route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+app.use(express.json());
 
 const port = 3000;
 
@@ -19,3 +21,4 @@ app.listen(port, () => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
